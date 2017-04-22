19348e8d894891970c97b225feaab70d469f2e60
grin <dmitriy.grinevich.qa@gmail.com>
2017-04-23 00:07:47 +0300

    added my logger

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,4 +1,6 @@
 import core.ADB;
+import core.MyLogger;
+import org.apache.log4j.Level;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,32 +9,10 @@ import java.io.InputStreamReader;
 public class Runner {
 
     public static void main(String[] args) throws IOException {
-        String someCommand = "adb devices";
-        String inputCommand = "";
-
-        //new UiSelector().resourceId("hello").text("text").checked(true);
-        //new UiSelector().makeUiObject();
-
-
-        while (true) {
-            System.out.println("Choose from list of commands and press enter: ");
-            System.out.println("-------------------------");
-            System.out.println("1 : show list of devices");
-            //System.out.println("2 : install app");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            inputCommand = reader.readLine();
-
-            if (inputCommand.equals("1")) {
-                System.out.println("-------------------------");
-                System.out.println(ADB.command("adb devices"));}
-            /*if (inputCommand.equals("2")) {
-                System.out.println("Enter devices: ");
-                BufferedReader readPaste = new BufferedReader(new InputStreamReader(System.in));
-                String paste = readPaste.readLine();
-                System.out.println("-------------------------");
-                System.out.println(ADB.command("adb " + paste));
-            }*/
-        }
+        MyLogger.log.setLevel(Level.ERROR);
+        MyLogger.log.debug("Test debug");
+        MyLogger.log.error("Test error");
+        MyLogger.log.info("Test info");
+        MyLogger.log.warn("Test warning");
     }
-
 }
